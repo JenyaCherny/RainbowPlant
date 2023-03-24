a478f25922fee8f9217d3c573139109686ee6f64
Jenya Cherny <jenya.cherny@gmail.com>
2023-03-23 20:56:07 -0400

    overall clean up

--- Models/Plant.cs
+++ Models/Plant.cs
@@ -7,11 +7,12 @@ public class Plant
     public int Id { get; set; }
     public string? PlantName { get; set; }
     public string? PlantDescription { get; set; }
+
     [DataType(DataType.Date)]
     public DateTime DateDelivered { get; set; }
     public int Quantity { get; set; }
-    [Range(0.01, 100.00,
-        ErrorMessage = "Price must be between 0.01 and 100.00")]
+    [RegularExpression(@"^\d+(\.\d{1,2})?$",
+       ErrorMessage = "The Unit Price cannot exceed 2 decimal places. Please Enter again")]
     public decimal UnitPrice { get; set; }
 }
 
